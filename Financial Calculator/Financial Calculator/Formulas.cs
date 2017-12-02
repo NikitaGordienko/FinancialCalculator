b40ef65ce0a419265231ca72e4bde866f7a16b7d
NikitaGordienko <33782013+NikitaGordienko@users.noreply.github.com>
2017-12-02 23:54:17 +0300

    Add files via upload

---
+++ Financial Calculator/Financial Calculator/Formulas.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Financial_Calculator
+{
+    class Formulas
+    {
+        //Здесь метод -> большая формула с кучей перегрузок
+    }
+}
