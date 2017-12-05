3e61b6f9b5a9ab97f66a6ddea7a1f65e4bb10c22
NikitaGordienko <33782013+NikitaGordienko@users.noreply.github.com>
2017-12-05 21:50:16 +0300

    v1.0
    Первая рабочая версия
    НЕ показывает проценты в нормальном формате
    НЕ тестировали параметр N

--- Financial Calculator/Financial Calculator/Rent.cs
+++ Financial Calculator/Financial Calculator/Rent.cs
@@ -17,30 +17,32 @@ namespace Financial_Calculator
         public double S; // Наращенная стоимость
         public bool flag; // true - постнумерандо, false - пренумерандо
 
-        public Rent(double R, double i, double n, double p, double m, double A, double S, bool flag, bool flagP, bool flagM)
+        public Rent(double R, double i, double n, double p, double m, double A, double S, bool flag)
         {
             this.R = R;
             this.i = i;
             this.n = n;
-            this.p = flagP == true ? p : 1;
-            this.m = flagM == true ? m : 1;
+            this.p = p;
+            this.m = m;
             this.A = A;
             this.S = S;
             this.flag = flag;
         }
 
-        public void CalculateRentCosts()
+        public void CalculateRentCosts() 
         {
             if (S != 0 & A == 0)
+            {
                 A = S * Math.Pow(1 + i / m, -m * n);
+            }
             else if (S == 0 & A != 0)
                 S = A * Math.Pow(1 + i / m, m * n);
+
             else if (S == 0 & A == 0)
             {
-                FindAccruedCost();
                 FindCurrentCost();
+                FindAccuredCost();
             }
-
 
         }
         // Определение недостающих элементов
@@ -49,25 +51,66 @@ namespace Financial_Calculator
                 
         }
 
-        public void FindAccruedCost()
+        public void FindAccuredCost()
         {
-            S = R / p * ((Math.Pow(1 + i / m, m * n) - 1) / (Math.Pow(1 + i / m, m / p) - 1));
+            if (flag == true)
+                S = R / p * ((Math.Pow(1 + i / m, m * n) - 1) / (Math.Pow(1 + i / m, m / p) - 1));
+            else
+                S = (R / p * ((Math.Pow(1 + i / m, m * n) - 1) / (Math.Pow(1 + i / m, m / p) - 1))) * Math.Pow(1 + i / m, m / p);
         }
 
         public void FindCurrentCost()
         {
-            A = R / p * ((1 - Math.Pow(1 + i / m, -m * n)) / (Math.Pow(1 + i / m, m / p) - 1));
+            if (flag==true)
+                A = R / p * ((1 - Math.Pow(1 + i / m, -m * n)) / (Math.Pow(1 + i / m, m / p) - 1));
+            else
+                A = (R / p * ((1 - Math.Pow(1 + i / m, -m * n)) / (Math.Pow(1 + i / m, m / p) - 1))) * Math.Pow(1 + i / m, m / p);
         }
 
         public void FindRentPayment()
         {
-            R = (S * p * (Math.Pow(1 + i / m, m / p) - 1)) / (Math.Pow(1 + i / m, m * n) - 1);
+            if (flag == true)
+            {
+                if (S == 0)
+                    R = (A * p * (Math.Pow(1 + i / m, m / p) - 1)) / (Math.Pow(1 + i / m, m * n) - 1);
+                else
+                    R = (S * p * (Math.Pow(1 + i / m, m / p) - 1)) / (1 - Math.Pow(1 + i / m, -m * n));
+            }
+            else
+            {
+                if (S == 0)
+                    R = (A * p * (Math.Pow(1 + i / m, m / p) - 1)) / (Math.Pow(1 + i / m, m * n) - 1);
+                else
+                    R = (S * p * (Math.Pow(1 + i / m, m / p) - 1)) / ((Math.Pow(1 + i / m, m * n) - 1) * Math.Pow(1 + i / m, m / p));
+            }
         }
 
         public void FindRentDuration()
         {
-            n = Math.Log((S * p * (Math.Pow(1 + i / m, m / p) - 1) / R) + 1) / (m * Math.Log(1 + i / m));
+            if (flag == true)
+            {
+                if (S == 0)
+                    n = Math.Log((S * Math.Pow(1 + i / m, -m * n) * p * (Math.Pow(1 + i / m, m / p) - 1) / R) + 1) / (m * Math.Log(1 + i / m)); 
+                else
+                    n = Math.Log((S * p * (Math.Pow(1 + i / m, m / p) - 1) / R) + 1) / (m * Math.Log(1 + i / m));
+            }
+            else
+            {
+                if (S == 0)
+                    n = Math.Log((S * Math.Pow(1 + i / m, -m * n) * p * (Math.Pow(1 + i / m, m / p) - 1) / (R * Math.Pow(1 + i / m, m / p))) + 1) / (m * Math.Log(1 + i / m));
+                else
+                    n = Math.Log((S * p * (Math.Pow(1 + i / m, m / p) - 1) / (R * Math.Pow(1 + i / m, m / p))) + 1) / (m * Math.Log(1 + i / m));
+            }        
         }
 
+        public void FindInterestRate()
+        {
+            double i_1 = (-n * (n - 1) / 2 + Math.Sqrt(Math.Pow(-n * (n - 1) / 2, 2) - 4 * (n * (n - 1) * (n - 2)) / 6 * (n - S / R))) / (2 * (n * (n - 1) * (n - 2)) / 6); 
+            double i_2 = (-n * (n - 1) / 2 - Math.Sqrt(Math.Pow(-n * (n - 1) / 2, 2) - 4 * (n * (n - 1) * (n - 2)) / 6 * (n - S / R))) / (2 * (n * (n - 1) * (n - 2)) / 6);
+            if (i_1 > 0)
+                i = i_1;
+            else
+                i = i_2;
+        }
     }
 }
--- Financial Calculator/Financial Calculator/RentPage.xaml.cs
+++ Financial Calculator/Financial Calculator/RentPage.xaml.cs
@@ -23,5 +23,151 @@ namespace Financial_Calculator
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            rbPost.IsChecked = true;
+            tbP.IsEnabled = false;
+            tbM.IsEnabled = false;
+        }
+
+        
+
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            int count_1;
+            int count_2;
+            double R = 0;
+            double I = 0; 
+            double N = 0;
+            int P;
+            int M;
+            double A = 0;
+            double S = 0;
+            bool annuity;
+
+
+            if (rbPost.IsChecked == true)
+                annuity = true;
+            else
+                annuity = false;
+
+            count_1 = 0;
+            //упростить через foreach
+            if (!string.IsNullOrWhiteSpace(tbR.Text) & double.TryParse(tbR.Text, out R))
+            {
+                R = Double.Parse(tbR.Text);
+                count_1++;
+            }        
+            if (!string.IsNullOrWhiteSpace(tbI.Text) & double.TryParse(tbI.Text, out I))
+            {
+                I = Double.Parse(tbI.Text)/100;
+                count_1++;
+            }
+            if (!string.IsNullOrWhiteSpace(tbN.Text) & double.TryParse(tbN.Text, out N))
+            {
+                N = Double.Parse(tbN.Text);
+                count_1++;
+            }
+            if (!string.IsNullOrWhiteSpace(tbP.Text) & int.TryParse(tbP.Text, out P))
+            {
+                P = Int32.Parse(tbP.Text);
+                count_1++;
+            }
+            else if (tbP.IsEnabled == false)
+            {
+                P = 1;
+                count_1++;
+            }
+            if (!string.IsNullOrWhiteSpace(tbM.Text) & int.TryParse(tbM.Text, out M) & tbM.IsEnabled == true)
+            {
+                M = Int32.Parse(tbM.Text);
+                count_1++;
+            }
+            else
+            {
+                M = 1;
+                count_1++;
+            }
+
+
+            count_2 = 0;
+            if (!string.IsNullOrWhiteSpace(tbA.Text) & double.TryParse(tbA.Text, out A))
+            {
+                A = Double.Parse(tbA.Text);
+                count_2++;
+            }
+            if (!string.IsNullOrWhiteSpace(tbS.Text) & double.TryParse(tbS.Text, out S))
+            {
+                S = Double.Parse(tbS.Text);
+                count_2++;
+            }
+
+
+            Rent ourRent = new Rent(R, I, N, P, M, A, S, annuity);
+            if (count_1 < 4)
+                MessageBox.Show("Введенных параметров не хватает для расчета параметров ренты!");
+            else if (count_1==4)
+            {
+                if (count_2 >= 1)
+                {
+                    if (ourRent.R == 0)
+                    {
+                        ourRent.CalculateRentCosts();
+                        ourRent.FindRentPayment();
+                        tbR.Text = ourRent.R.ToString();
+                    }
+                    else if (ourRent.i==0)
+                    {
+                        ourRent.CalculateRentCosts();
+                        ourRent.FindInterestRate();
+                        tbI.Text = ourRent.i.ToString();
+                    }
+                    else if (ourRent.n==0)
+                    {
+                        ourRent.CalculateRentCosts();
+                        ourRent.FindRentDuration();
+                        tbN.Text = ourRent.n.ToString(); // изменить формат на проценты
+                    }
+                }
+                else
+                    MessageBox.Show("Введенных данных не хватает для вычисления параметров ренты!");
+            }
+            else if (count_1==5)
+            {
+                if (count_2 == 2)
+                    MessageBox.Show("Все параметры ренты уже известны!");
+                if (count_2 < 2)
+                {
+                    ourRent.FindCurrentCost();
+                    ourRent.FindAccuredCost();
+                    tbA.Text = ourRent.A.ToString();
+                    tbS.Text = ourRent.S.ToString();
+                }
+            }
+
+        }
+
+        private void cbP_Checked(object sender, RoutedEventArgs e)
+        {
+            tbP.IsEnabled = true;
+        }
+
+        private void cbP_Unchecked(object sender, RoutedEventArgs e)
+        {
+            tbP.IsEnabled = false;
+        }
+
+        private void cbM_Checked(object sender, RoutedEventArgs e)
+        {
+            tbM.IsEnabled = true;
+        }
+
+        private void cbM_Unchecked(object sender, RoutedEventArgs e)
+        {
+            tbM.IsEnabled = false;
+        }
+
+        
     }
 }
